f1b2806ec98edfd4446cdbf5589d63ff5b00713c
Your Name <masoudabbasnejad1@gmail.com>
2026-05-14 07:22:24 -0700

    howe page top off black element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,12 @@ import Sidebar from "@/component/admin/sidebar/sidebar";
 import AdminSidebarNavbar from "@/component/admin/adminSidebarNavbar";
 import store from "@/redux/store/store";
 import {Provider} from "react-redux";
+import HomeComponent from "@/component/app/home/home-component";
 
 export default function Home() {
     return (
       <div>
-          Homeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+          <HomeComponent/>
       </div>
     );
 }
