b9d84efdd4083c79cfd90a28a7fe8d7e0accbcc6
Your Name <masoudabbasnejad1@gmail.com>
2026-05-14 08:01:32 -0700

    Home page top off black has done (responsive)

--- src/component/app/home/home-component.tsx
+++ src/component/app/home/home-component.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 
-export default function HomeComponent() {
+export default function TopHome() {
     return (
+        // mother component
         <div className={"bg-stone-950 w-full flex flex-col-3 justify-start  md:justify-center md:h-10 items-center "}>
 
              {/*invisible Div*/}
@@ -9,15 +10,14 @@ export default function HomeComponent() {
              </div>
 
              {/*/!*Text Center*!/*/}
-             <div className={" w-full md:w-4/10 flex flex-col-2 md:justify-center items-center"}>
+             <div className={" w-full md:w-4/10 flex flex-col-2 md:justify-center sm:justify-center items-center"}>
 
-                 <span className={"w-5/10 text-white text-nowrap md:text-md text-xs whitespace-normal break-normal"}>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%! </span>
-                 <span className={" w-1/10  text-center text-xs text-nowrap md:text-md text-white font-semibold cursor-pointer"}> ShopNow</span>
+                 <span className={" w-10/10 text-white text-nowrap md:text-md text-xs whitespace-normal break-normal"}>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%! <span className={" font-semibold ms-2 cursor-pointer"}>ShopNow</span></span>
 
              </div>
 
              {/*English drop down*/}
-             <div className={"w-1/10 md:w-3/10 flex flex-col justify-start"}>
+             <div className={" w-1/10 md:w-3/10 flex flex-col justify-start"}>
                  <div className={" md:w-2/4 flex flex-col-2 justify-center"}>
                      {/*english*/}
                      <div className={" md:w-6/5 text-center flex justify-end"}>
