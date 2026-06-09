f2e7e7c8ea2b7b17c3633505fac23157ecbdec86
Your Name <masoudabbasnejad1@gmail.com>
2026-06-09 15:55:19 -0700

    cart

---
+++ src/component/app/wishlist/Wishlist.tsx
@@ -0,0 +1,15 @@
+import Footer from '@/component/app/home/footer/footer';
+import HomeNavbar from '@/component/app/home/navbar/home-navbar';
+import Section4 from '@/component/app/home/section4/section4';
+import TopHome from '@/component/app/home/top-off/home-component';
+
+export default function Wishlist() {
+  return (
+    <>
+      <TopHome />
+      <HomeNavbar />
+      <Section4/>
+      <Footer />
+    </>
+  );
+}
