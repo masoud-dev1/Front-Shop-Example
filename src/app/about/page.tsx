4dc80588d0ea86b56f5c7e21539986858a0c8741
Your Name <masoudabbasnejad1@gmail.com>
2026-06-12 03:58:16 -0700

    final

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,6 +4,8 @@ import Footer from "@/component/app/home/footer/footer";
 import AboutSection1 from "@/component/app/about/section1/About-Section1";
 import Image from "next/image";
 import AboutSection2 from "@/component/app/about/section2/About-Section2";
+import AboutScroll from "@/component/app/about/section2/scroll/About-Scroll";
+import Section8 from "@/component/app/home/section8/section8";
 
 export default function AboutPage() {
     return (
@@ -12,6 +14,16 @@ export default function AboutPage() {
             <HomeNavbar/>
             <AboutSection1/>
             <AboutSection2/>
+
+            <div className={" w-4/5 mx-auto mt-40"}>
+                <AboutScroll/>
+            </div>
+
+            <div className={"mt-10"}>
+                <Section8 />
+            </div>
+
+
             <Footer/>
         </>
     )
